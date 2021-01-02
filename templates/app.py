83414be50f02f097eda656e2d3cfac749fb03306
Juan Cruz <jc23729@gmail.com>
2021-01-02 17:36:04 -0500

    added a venv file

---
+++ templates/app.py
@@ -0,0 +1,12 @@
+from flask import flask
+
+app= Flask(__name__)
+
+
+"""You want to setup a template directory, a folder where your going to have repeatable code
+my-project-directory/
+  venv/
+  app.py
+  templates/
+    hello.html
+"""
